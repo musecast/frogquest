b2ec321069635b0155d9d98fe28b41d9d747ae1a
Muse <jasonwmuse@gmail.com>
2024-08-19 22:10:27 -0400

    frog quest

--- scenes/tutorialwobble.gd
+++ scenes/tutorialwobble.gd
@@ -6,5 +6,6 @@ var heartoffset = randf_range(-0.2, 0.2)  # Random offset for each heart
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var time_passed = (Time.get_ticks_msec() / 1000.0) + heartoffset
-	rotation = sin(time_passed * speed) * angle_range
+	if $"../../Player/Trajectory".endGame == 0:
+		var time_passed = (Time.get_ticks_msec() / 1000.0) + heartoffset
+		rotation = sin(time_passed * speed) * angle_range
